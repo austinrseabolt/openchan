c306ecf1ed5d55aa1a63aac81efcd196d226e6c4
Austin Seabolt <austinseabolt@Austins-iMac.local>
2020-06-24 15:23:17 -0400

    Changes to admin interface

--- src/mainapp/admin.py
+++ src/mainapp/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 from .models import *
+
 admin.site.register(OpenChan)
-admin.site.register(Post)
-admin.site.register(Board)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('local_id', 'parent_board', 'parent_post',)
+    pass
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    exclude = ('post_counter',)
