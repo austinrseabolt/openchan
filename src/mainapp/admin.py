08f374d141740e72f1243718c1bf73d1963b0c5c
Austin Seabolt <austinrseabolt@gmail.com>
2020-06-29 12:47:59 -0400

    Added News

--- src/mainapp/admin.py
+++ src/mainapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(OpenChan)
-
+admin.site.register(News)
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     exclude = ('local_id', 'parent_board', 'parent_post',)
--- src/mainapp/models.py
+++ src/mainapp/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class OpenChan(models.Model):
     site_name = models.CharField(max_length=20, default="OpenChan", unique=True)
     site_desc = models.CharField(max_length = 50, default="The Open Source Chan Software")
+    custom_cursor = models.ImageField(blank=True, null=True, upload_to='cursor/')
     
     def __str__(self):
         return("Site Settings")
@@ -11,11 +12,21 @@ class OpenChan(models.Model):
         verbose_name = 'Openchan Instance'
         verbose_name_plural = 'Openchan Instance'
 
+class News(models.Model):
+    news_subject = models.CharField(max_length=30)
+    news_content = models.TextField()
+    news_date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return(self.news_subject)
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
 
 class Board(models.Model):
     board_url = models.CharField(max_length=4, primary_key=True)
     board_description = models.CharField(max_length=30)
     post_counter = models.BigIntegerField(default = 0)
+    custom_cursor = models.ImageField(blank=True, null=True, upload_to='cursor/')
 
     def __str__(self):
         return str("/" + self.board_url + "/ - " + self.board_description)
@@ -26,7 +37,7 @@ class Post(models.Model):
     post_name = models.CharField(max_length=30, default="Anonymous")
     post_subject = models.CharField(max_length=50, blank=True)
     post_content = models.TextField()
-    post_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    post_image = models.ImageField(upload_to='images/' , blank=True, null=True)
     post_date = models.DateTimeField(auto_now_add=True, blank=True)
     parent_post = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
     parent_board = models.ForeignKey("Board", null=True, blank=True, on_delete=models.CASCADE)
